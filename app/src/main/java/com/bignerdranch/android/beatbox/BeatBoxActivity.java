7b6370f06766fa2213b04e107e7fc15596899f3d
cometomyrise <bodiasolo322@gmail.com>
2018-11-15 16:06:16 +0200

    added 2 files: SingleFragmentActivity.java & activity_fragment.xml from CriminalIntent project
    reformatted BeatBoxActivity as descendant of SingleFragmentActivity

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -1,13 +1,14 @@
 package com.bignerdranch.android.beatbox;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+    protected Fragment createFragment() {
+
+        //create new instance of BeatBoxFragment and sent it to SingleFragmentActivity as Fragment
+        //where it uses in onCreate by FragmentManager 
+        return BeatBoxFragment.newinstance();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    /*
+    static method to get instance of this fragment from other classes (activities)
+     */
+    public static BeatBoxFragment newinstance() {
+
+        //will be filled soon
+        return new BeatBoxFragment();
+    }
+
+}
