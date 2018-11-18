a8d9050e249f16402a25861053388851f52c34b4
cometomyrise <bodiasolo322@gmail.com>
2018-11-18 04:11:03 +0200

    minor fix of mixing filenames between each other while scrolling
    Signed-off-by: cometomyrise <bodiasolo322@gmail.com>

--- app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.beatbox;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /*
 VIEW MODEL CLASS
 (final part of MVVM pattern, maybe)
@@ -26,9 +29,16 @@ public boolean isDead() {
 
 WHY we need that middle level?
 Steve McConnell "Code Complete"
- */
 
-public class SoundViewModel {
+UPD:
+to display file_names correctly without mixing while scrolling
+we need to implement Observable interface & override all things
+but there's easier way:
+    - make SoundViewModel be inherited from BaseObservable class
+    - pin methods, using in binding with @Bindable annotation
+    - call notifyChange() or notifyPropertyChanged(int propertyId) each time changes appears
+ */
+public class SoundViewModel extends BaseObservable {
     //sound - contact with model
     //beatbox - contact with asset manager
     private Sound mSound;
@@ -45,9 +55,11 @@ public class SoundViewModel {
 
     public void setSound(Sound sound) {
         mSound = sound;
+        notifyChange();
     }
 
     //title getter (model.Name -> VM -> view.Text)
+    @Bindable
     public String getTitle() {
         return mSound.getName();
     }
